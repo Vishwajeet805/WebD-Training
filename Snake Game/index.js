9d9b7a43646f8ed253ba5938c2f018a0f41c62ab
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2026-01-27 22:19:38 +0530

    Snake game Added

---
+++ Snake Game/index.js
@@ -0,0 +1,26 @@
+// let arr=[1,2,3,4,5]
+// arr.push(6)
+// // arr.shift()
+// arr.pop()
+// console.log(arr);
+
+// function sum(){
+//     console.log("hehehe");
+   
+// }
+// sum()
+// let sum=()=>{
+//     console.log("hello");
+// }
+// sum()
+// console.log("hii");
+
+// setInterval(()=>{
+//     console.log("hello");
+    
+// },3000)
+
+// setTimeout(()=>{
+//     console.log("hello");
+    
+// },3000)
